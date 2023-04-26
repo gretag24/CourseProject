96504625d2d8e12b21ef46090ff4eaf4ddb71e2d
Khushi Patel <khushi.patel@marquette.edu>
2023-04-26 16:31:39 -0500

    Create Customers, Appointment, and CustomerAppointments

---
+++ CourseProject/Models/Customers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models
+{
+    public class Customers
+    {
+
+        public List<Customer> customers { get; set; }
+
+        public Customers()
+        {
+            customers = new List<Customer>();
+        }
+
+        public Customer Authenticate(string username, string password)
+        {
+            Customer c = customers.Where(o => o.Username == username).First();
+            if (c! = null)
+            {
+                if (c.Password == password)
+                {
+                    return c;
+                }
+                else
+                {
+                    return null;
+                }
+
+            }
+            return null;
+
+        }
+       
+    }
+}

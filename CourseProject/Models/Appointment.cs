bb33645ab5420e356bda3bd8890a7b0818929408
Greta Geheren <greta.geheren@marquette.edu>
2023-05-02 17:35:48 -0500

    program

---
+++ CourseProject/Models/Appointment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject.Models
+{
+    public class Appointment
+    {
+        private static int autoIncriment; 
+
+        public int Id { get; set; }
+
+        public DateTime date { get; set; }
+    
+    
+    
+    
+    
+    
+    
+    }
+}

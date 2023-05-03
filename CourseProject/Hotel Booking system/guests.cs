d724acca97dc77f5ff92675c31cba1981060d2f3
Niamh Gardiner <niamh.gardiner@marquette.edu>
2023-05-02 19:09:19 -0500

    guests finished

--- CourseProject/Hotel Booking system/guests.cs
+++ CourseProject/Hotel Booking system/guests.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace CourseProject.Hotel_Booking_system
 {
-    internal class guests
+    public class Guest
     {
+
+        public List<Guest> guests { get; set; }
+
+        public Guests()
+        {
+            guest = new List<Guest>();
+        }
+
+        public Guest Authenticate(string username, string password)
+        {
+            var g = guest.Where(o => o.Username == username) && (o.Password == password);
+
+            if (g.Count() > 0)
+            {
+                return g.First();
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+
     }
 }

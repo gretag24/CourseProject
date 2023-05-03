464c8a2ee965d0db5b1d7c51a315a3b83140804f
Nicole Gardiner <nicole.gardiner@marquette.edu>
2023-05-02 19:07:59 -0500

    Revert "Reservations update"
    This reverts commit fa0b9ab09f11657dff86b728d52570973fd3fada.

--- CourseProject/Hotel Booking system/reservations.cs
+++ CourseProject/Hotel Booking system/reservations.cs
@@ -8,19 +8,5 @@ namespace CourseProject.Hotel_Booking_system
 {
     internal class reservations
     {
-        public class reservations
-        {
-            private static int autoIncreament;
-            public int Id { get; set; }
-
-            public DateTime date { get; set; }
-
-
-            public reservations()
-            {
-                autoIncreament++;
-                Id = autoIncreament;
-            }
-        }
     }
 }
--- CourseProject/LoginMenu.cs
+++ CourseProject/LoginMenu.cs
@@ -9,5 +9,4 @@ namespace CourseProject
     internal class LoginMenu
     {
     }
-
 }
